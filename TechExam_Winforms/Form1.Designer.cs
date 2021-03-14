2bbea81792ee0fa8357a14d8f6126958aa5a1f5f
MANILA\jcalma <jcalma@equicom.com>
2021-03-14 21:54:19 +0800

    Add project files.

---
+++ TechExam_Winforms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace TechExam_Winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.rtxtDetails = new System.Windows.Forms.RichTextBox();
+            this.mTxtPassword = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(59, 14);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(280, 23);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.Text = "intevalue.machineproblem2021@gmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(644, 13);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // rtxtDetails
+            // 
+            this.rtxtDetails.Location = new System.Drawing.Point(17, 67);
+            this.rtxtDetails.Name = "rtxtDetails";
+            this.rtxtDetails.Size = new System.Drawing.Size(702, 171);
+            this.rtxtDetails.TabIndex = 5;
+            this.rtxtDetails.Text = "";
+            // 
+            // mTxtPassword
+            // 
+            this.mTxtPassword.Location = new System.Drawing.Point(433, 12);
+            this.mTxtPassword.Name = "mTxtPassword";
+            this.mTxtPassword.PasswordChar = '*';
+            this.mTxtPassword.Size = new System.Drawing.Size(187, 23);
+            this.mTxtPassword.TabIndex = 6;
+            this.mTxtPassword.Text = "m@chinepr0blem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 280);
+            this.Controls.Add(this.mTxtPassword);
+            this.Controls.Add(this.rtxtDetails);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox rtxtDetails;
+        private System.Windows.Forms.MaskedTextBox mTxtPassword;
+    }
+}
+
---
+++ TechExam_Winforms/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechExam_Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //using (MyApiClient client = new MyApiClient("https://api.github.com/"))
+            //{
+            //    string response = client.GetAsync("users/intevaluetesting").Result;
+            //    rtxtDetails.Text = response.ToString();
+            //}
+
+            HttpWebRequest webRequest = System.Net.WebRequest.Create("https://api.github.com/users/intevaluetesting") as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.Method = "GET";
+                webRequest.UserAgent = "Anything";
+                webRequest.ServicePoint.Expect100Continue = false;
+
+                try
+                {
+                    using (StreamReader responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream()))
+                    {
+                        string reader = responseReader.ReadToEnd();
+                        var jsonobj = JsonConvert.DeserializeObject(reader);
+                        rtxtDetails.Text = jsonobj.ToString();
+                    }
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+        }
+    }
+}
